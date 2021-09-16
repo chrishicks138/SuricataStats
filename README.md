e4264910dc91c9693f2526abc1562a317322d6a9
chrishicks138 <chrishicks138@gmail.com>
2021-09-16 08:46:39 +0000

    Cleaned up routes.py

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # SuricataStats
+
+Needs Suricata and Python
+
+
+INSTALL
+cd SuricataStats
+pip3 install -r requirements.txt
+
+RUN
+cd SuricataStats
+python3 routes.py
--- routes.py
+++ routes.py
@@ -5,37 +5,45 @@ app = Flask(__name__)
 
 first = '| Total                     |'
 date = 'Date: '
-listvalues = []
+
 labels = []
-colors = []
-colorint = {}
-with open('/var/log/suricata/stats.log') as logfile:
-    lines = logfile.readlines()
-    keys = [ ]
+
+def logParse():
+    colors = []
+    colorint = {}
+    keys = []
     dictvalues = {}
-    for line in lines[::128]:
-        if date in line:
-            labels.append(line.split(' (up')[0].replace('Date: ', '').replace('-', '').replace(' ', '_'))
-        if first in line:
-            line = line.replace(first, ':').replace('\n', '').replace(' ', '')
-            key  = line.split(':')[0]
-            value = int(line.split(':')[1])
-            if key not in keys:
-                for x in range(3):
-                    colorint[x] = random.randint(0,255)
-                string = "rgba("+str(colorint[0])+","+str(colorint[1])+","+str(colorint[2])+",1)"
-                keys.append(key)
-                colors.append(string)
-            try:
-                dictvalues[key].append(value)
-            except:
-                dictvalues[key] = []
+    with open('/var/log/suricata/stats.log') as logfile:
+        lines = logfile.readlines()
+        for line in lines[::128]:
+            if date in line:
+                line = line.split(' (up')[0].replace('Date: ', '').replace('-', '').replace(' ', '_')
+                if line not in labels:
+                    labels.append(line)
+            if first in line:
+                line = line.replace(first, ':').replace('\n', '').replace(' ', '')
+                key  = line.split(':')[0]
+                value = int(line.split(':')[1])
+                if key not in keys:
+                    for x in range(3):
+                        colorint[x] = random.randint(0,255)
+                    string = "rgba("+str(colorint[0])+","+str(colorint[1])+","+str(colorint[2])+",1)"
+                    keys.append(key)
+                    colors.append(string)
+                try:
+                    dictvalues[key].append(value)
+                except:
+                    dictvalues[key] = []
+    return dictvalues, colors
 
 @app.route('/')
 def hello_world():
+    dictvalues = logParse()
+    colors = dictvalues[1]
+    dictvalues = dictvalues[0]
     values = list(dictvalues.values())
-    legends = keys
-    counted = [i for i in range(len(keys))]
+    legends = list(dictvalues.keys())
+    counted = [i for i in range(len(list(dictvalues.keys()))) ]
     data = [ legends, labels, values ]
     return render_template('index.html', colors=colors, data=data, counted=counted)
 
